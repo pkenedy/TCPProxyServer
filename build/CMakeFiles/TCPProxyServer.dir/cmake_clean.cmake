aab052dfde6e0b6c3ba9d4bfa016275c5647ecb1
Paul Kenedy <pkenedy48@gmail.com>
2024-10-30 12:31:22 +0300

    pages updates

---
+++ build/CMakeFiles/TCPProxyServer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TCPProxyServer.dir/tcp_proxy_server.cpp.o"
+  "CMakeFiles/TCPProxyServer.dir/tcp_proxy_server.cpp.o.d"
+  "TCPProxyServer.exe"
+  "TCPProxyServer.pdb"
+  "libTCPProxyServer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TCPProxyServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
